2373b999dd4cba90feb96add98e090b714ae883e
JianWang1123 <103565951+JianWang1123@users.noreply.github.com>
2025-07-30 18:33:54 +0000

    Deploying to gh-pages from @ JianWang1123/mathbiojw@de6b5e91a6b8f307985960dcd8bc9938a4c492dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/mathbiojw/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/mathbiojw/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-my-first-first-author-journal-paper-publised-at-food-research-international-wang-j-hashem-i-bhonsale-s-verheyen-d-luo-h-amp-amp-van-impe-j-f-2025-individual-based-modelling-ibm-in-food-microbiology-a-comprehensive-guideline-food-research-international-116408",
+          title: 'My first first-author journal paper publised at Food Research International: Wang, J., Hashem,...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
