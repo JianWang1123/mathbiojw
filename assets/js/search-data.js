7df2ceb4514fdd07e497023e4627fd7fc4d618c0
JianWang1123 <103565951+JianWang1123@users.noreply.github.com>
2025-07-31 12:06:42 +0000

    Deploying to gh-pages from @ JianWang1123/mathbiojw@88d7fca3e01090cbfed309ae232d4314557972de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/mathbiojw/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/mathbiojw/people/";
-          },
         },{id: "nav-submenus",
           title: "submenus",
           description: "",
